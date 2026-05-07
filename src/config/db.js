5ea3e465c265ca42b95fa5db3132ea784f5e4641
Sumit <Githubsummitproject@2022>
2026-05-07 17:42:44 +0530

    initial commit

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+async function  connectDB() {
+
+    try {
+
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("database connected");
+        
+    } catch (error) {
+        console.error("database failed to connect",error);
+
+    }
+    
+}
+
+module.exports = connectDB;
